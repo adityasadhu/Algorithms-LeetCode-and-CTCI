607de82dac645c85daaf45bd97429346e143ff57
Aditya Sadhu <aditya.hf96@gmail.com>
2018-08-31 13:29:02 -0400

    ArrayProblem MaximumSubArray

--- src/leetcode/arrays/RemoveElement.java
+++ src/leetcode/arrays/RemoveElement.java
@@ -14,11 +14,22 @@ public class RemoveElement {
         return array.length - count;
     }
 
+    static public int answer2(int[] array, int value){
+        int i = 0;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] != value) {
+                array[i] = array[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
     public static void main(String[] args) {
 
         int[] array = {0, 1, 2, 2, 3, 0, 4, 2};
 
-        int answer = answer(array, 2);
+        int answer = answer2(array, 2);
 
         System.out.println(answer);
     }
