589ae9ef0e2b51bcd6c9c03d0a2b55380cbda146
Aditya Sadhu <aditya.hf96@gmail.com>
2018-09-01 15:20:35 -0400

    added Array Problem rotateMatrix

---
+++ src/Ctci/ArraysAndStrings/RotateMatrix.java
@@ -0,0 +1,46 @@
+package Ctci.ArraysAndStrings;
+
+public class RotateMatrix {
+
+
+    static public int[][] answer(int[][] matrix) {
+
+
+        if (matrix.length == 0 || matrix.length != matrix[0].length) {
+            return null;
+        }
+        int length = matrix.length;
+
+        for (int layer = 0; layer < length / 2; layer++) {
+            int first = layer;
+            int last = length - 1 - layer;
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = matrix[first][i];
+                matrix[first][i] = matrix[last - offset][first];
+                matrix[last - offset][first] = matrix[last][last - offset];
+                matrix[last][last - offset] = matrix[i][last];
+                matrix[i][last] = top;
+
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+
+
+        int[][] test = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+        int[][] answer = answer(test);
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer.length; j++) {
+                System.out.print(answer[i][j]);
+            }
+            System.out.println("");
+
+        }
+
+    }
+}
