a9fc00d29f5aef72a4591d67d28a144a944124c5
Aditya Sadhu <aditya.hf96@gmail.com>
2018-09-02 13:02:33 -0400

    added LinkedListProblem removeLinkedListCycle in LinkedListCycle

--- src/leetcode/linkedlists/LinkedlistCycle.java
+++ src/leetcode/linkedlists/LinkedlistCycle.java
@@ -1,20 +1,55 @@
 package leetcode.linkedlists;
 
-public class LinkedlistCycle extends LinkedList{
+import java.util.HashSet;
 
-    public boolean hasCycle(Node head){
+public class LinkedlistCycle extends LinkedList {
+
+    public boolean hasCycle(Node head) {
         Node slow = head;
         Node fast = head;
 
-        while (fast!=null && fast.next!=null){
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
 
-            if(slow ==fast){
+            if (slow == fast) {
                 return true;
             }
         }
         return false;
+    }
+
+
+    //with HashSet
+
+    public boolean hasCycleHashSet(Node head) {
+
+        HashSet<Node> hashSet = new HashSet<>();
+
+        Node current = head;
+        Node hasCycleNode = null;
+
+        while (current != null) {
+            if(hashSet.contains(current)){
+                hasCycleNode = current;
+                removeCycle(head, current);
+                return true;
+            }else{
+                hashSet.add(current);
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+
+    public void removeCycle(Node head, Node node){
+        Node current = head;
+
+        while (current.next == node){
+            current = current.next;
+        }
+        current.next = null;
     }
 
     public static void main(String[] args) {
@@ -24,9 +59,11 @@ public class LinkedlistCycle extends LinkedList{
         linkedlistCycle.append(20);
         linkedlistCycle.append(30);
         linkedlistCycle.append(40);
-       // head.next.next.next.next = head;
-        boolean result = linkedlistCycle.hasCycle(head);
+        head.next.next.next.next = head;
+        boolean result = linkedlistCycle.hasCycleHashSet(head);
         System.out.println(result);
+        boolean result2 = linkedlistCycle.hasCycleHashSet(head);
+        System.out.println(result2);
 
 
 
