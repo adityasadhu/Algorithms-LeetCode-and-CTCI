d2058c5fdec8187e1ea38d4f206ed6b8df8111dc
Aditya Sadhu <aditya.hf96@gmail.com>
2018-09-02 01:05:42 -0400

    added stackUsingLinkedList

---
+++ src/leetcode/linkedlists/StackLinkedList.java
@@ -0,0 +1,54 @@
+package leetcode.linkedlists;
+
+
+public class StackLinkedList {
+
+    Node top;
+
+    public StackLinkedList() {
+        top = null;
+    }
+
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+
+        if (top == null) {
+            newNode.next = null;
+        } else {
+            newNode.next = top;
+        }
+        top = newNode;
+    }
+
+    public void pop() {
+        if (top == null) {
+            return;
+        }
+        top = top.next;
+    }
+
+    public void peek() {
+        if (top == null) {
+            return;
+        }
+        System.out.println(top.data);
+    }
+
+    public void printStack() {
+        Node current = top;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        StackLinkedList stackLinkedList = new StackLinkedList();
+        stackLinkedList.push(10);
+        stackLinkedList.push(20);
+        stackLinkedList.printStack();
+        stackLinkedList.peek();
+    }
+}
