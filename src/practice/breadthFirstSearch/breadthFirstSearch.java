d6bc8cd084e008cd1b99968f3667578e7ae789f9
Aditya Sadhu <aditya.hf96@gmail.com>
2018-12-28 02:09:21 -0500

    String Problem compareVersionNumbers and Array Problem twoSum

---
+++ src/practice/breadthFirstSearch/breadthFirstSearch.java
@@ -0,0 +1,41 @@
+/*
+Graphs
+Graph = G = { E, V }
+
+One of the problems is how we represent graphs
+adjacency lists is a good way to represent them
+
+adjacency lists
+    array Adj of |v| - > linked lists
+
+    for each vertex u belongs v
+    adj[u] stores neighbors
+
+
+Graph search applications:
+    -web crawling
+    -social networking (example friends of friends)
+    -network broadcast
+    -garbage collection
+
+
+Breadth -first search
+    -visit all the nodes reachable from given vertex
+    -in O(V + E)
+    -Careful to avoid duplicates
+
+
+
+
+*/
+package practice.breadthFirstSearch;
+
+public class breadthFirstSearch {
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/practice/depthFirstSearch/depthFirstSearch.java
@@ -0,0 +1,11 @@
+package practice.depthFirstSearch;
+
+public class depthFirstSearch {
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
