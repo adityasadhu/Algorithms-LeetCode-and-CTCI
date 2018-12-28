2ca9ee5349dd068f2f0ff20569196581269befe6
aditya sadhu <aditya.hf96@gmail.com>
2018-12-28 18:13:51 -0500

    ArrayProblem productOfArrayExceptSelf

---
+++ src/practice/amazon/productOfArrayExceptSelf.java
@@ -0,0 +1,26 @@
+package practice.amazon;
+
+public class productOfArrayExceptSelf {
+
+
+    public int[] answer(int[] nums) {
+
+        int n = nums.length;
+        int[] result = new int[n];
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= right;
+            right *= nums[i];
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
