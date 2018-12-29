bc0bdfb4a5bec5f3dd49b679d33715ceb7547663
Aditya Sadhu <aditya.hf96@gmail.com>
2018-12-29 02:13:51 -0500

    Toy Implementation of BreadthFirstSearch

--- src/practice/breadthFirstSearch/breadthFirstSearch.java
+++ src/practice/breadthFirstSearch/breadthFirstSearch.java
@@ -30,12 +30,53 @@ Breadth -first search
 */
 package practice.breadthFirstSearch;
 
+import practice.Node;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class breadthFirstSearch {
 
+    public static String bfs(Node root, int toFind) {
 
+        Queue<Node> queue = new LinkedList<>();
+        HashSet<Node> hashSet = new HashSet<>();
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            Node temp = queue.poll();
+            hashSet.add(temp);
+            if (temp.data == toFind) {
+                return "nice nice found using BFS";
+            }
+
+            if (temp.left != null && !hashSet.contains(temp.left))
+                queue.offer(temp.left);
+
+            if (temp.right != null && !hashSet.contains(temp.left))
+                queue.offer(temp.right);
+
+        }
+        return "the item you are searching is not there in the graph";
+    }
 
 
     public static void main(String[] args) {
+/*                             1
+                ------------------------------
+                2                             5
+         ----------------               ---------------
+        3                 4             6              7
+                                                   ---------
+                                                   8        null
+ */
+        Node root = new Node(1,
+                new Node(2, new Node(3), new Node(4)),
+                new Node(5, new Node(6), new Node(7, new Node(8), null)));
+
+        System.out.println(bfs(root, 2));
 
     }
 }
