20298faf9d1bb0a3fdf182d5ebb5b8cce786201d
Aditya Sadhu <aditya.hf96@gmail.com>
2018-12-29 11:55:49 -0500

    Implemented BreadthFirstSearch

--- src/practice/breadthFirstSearch/breadthFirstSearch.java
+++ src/practice/breadthFirstSearch/breadthFirstSearch.java
@@ -32,10 +32,65 @@ package practice.breadthFirstSearch;
 
 import practice.Node;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class breadthFirstSearch {
+
+    public int v;
+
+    public LinkedList<Integer>[] adjacencyList;
+
+    public breadthFirstSearch(int v) {
+        this.v = v;
+        adjacencyList = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adjacencyList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int a, int b) {
+        adjacencyList[a].add(b);
+    }
+
+    public boolean bfs(int vertex, int toFind, int graphSize) {
+
+        boolean[] visited = new boolean[graphSize];
+
+        return bfsUtil(visited, vertex, toFind);
+    }
+
+    public boolean bfsUtil(boolean[] visited, int vertex, int toFind) {
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(vertex);
+
+        Iterator<Integer> iterator = null;
+
+        while (!queue.isEmpty()) {
+
+            int temp = queue.poll();
+
+            if (visited[temp] == true)
+                continue;
+
+            visited[temp] = true;
+
+            if (temp == toFind)
+                return true;
+
+            iterator = adjacencyList[temp].listIterator();
+
+            while (iterator.hasNext()) {
+                int next = iterator.next();
+                if (visited[next] != true)
+                    queue.offer(next);
+            }
+        }
+
+        return false;
+    }
 
     public static String toyImplementationbfs(Node root, int toFind) {
 
@@ -62,6 +117,7 @@ public class breadthFirstSearch {
 
 
     public static void main(String[] args) {
+
 /*                             1
                 ------------------------------
                 2                             5
@@ -73,6 +129,17 @@ public class breadthFirstSearch {
         Node root = new Node(1,
                 new Node(2, new Node(3), new Node(4)),
                 new Node(5, new Node(6), new Node(7, new Node(8), null)));
+
+        breadthFirstSearch b = new breadthFirstSearch(4);
+
+        b.addEdge(0, 1);
+        b.addEdge(0, 2);
+        b.addEdge(1, 2);
+        b.addEdge(2, 0);
+        b.addEdge(2, 3);
+        b.addEdge(3, 3);
+
+        System.out.println(b.bfs(0, 3, 4));
 
         System.out.println("\n" + toyImplementationbfs(root, 8));
 
