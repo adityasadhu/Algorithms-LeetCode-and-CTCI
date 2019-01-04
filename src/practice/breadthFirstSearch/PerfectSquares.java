c5a440bfadda57e64ef98fdb451dd62a410afc73
aditya sadhu <aditya.hf96@gmail.com>
2019-01-04 13:29:47 -0500

    GraphProblem perFectSquares another solution, firstNonRepeatingCharacterFromAStream, KthLargestElementInAnArray

--- src/practice/breadthFirstSearch/PerfectSquares.java
+++ src/practice/breadthFirstSearch/PerfectSquares.java
@@ -14,6 +14,8 @@
 
 package practice.breadthFirstSearch;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -42,7 +44,46 @@ public class PerfectSquares {
         return -1;
     }
 
+    public static int differentApproach(int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> perfectSquares = new ArrayList<>();
+        HashSet<Integer> visited = new HashSet<>();
+        int level = 1;
+        for (int i = 1; i * i <= n; i++) {
+            if (i * i == n)
+                return level;
+            perfectSquares.add(i * i);
+        }
+        for (Integer a : perfectSquares) {
+            queue.offer(a);
+        }
+
+        while (!queue.isEmpty()) {
+            level++;
+            int size = queue.size();
+            while (size > 0) {
+                int temp = queue.poll();
+                for (int s : perfectSquares) {
+                    System.out.println("level is" + level + queue);
+                    int check = temp + s;
+
+                    if (check == n)
+                        return level;
+
+                    else if (check < n && !visited.contains(check)) {
+                        visited.add(check);
+                        queue.offer(check);
+                    } else if (check > n) {
+                        continue;
+                    }
+                }
+                size--;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        System.out.println(numSquares(40));
+        System.out.println(numSquares(12));
     }
 }
