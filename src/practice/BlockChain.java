3a9bd51f3bcdcc46d62769f8c343fc313c281de8
Aditya Sadhu <aditya.hf96@gmail.com>
2019-01-30 19:39:24 -0500

    refactor BlockChain

--- src/practice/BlockChain.java
+++ src/practice/BlockChain.java
@@ -14,17 +14,13 @@ public class BlockChain {
         for (int i = 0; i < result.length; i++) {
             sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
         }
-
         return sb.toString();
     }
 
-
     static public String getLatestBlock(int[] startBalances, int[][] pendingTransactions, int blockSize) throws NoSuchAlgorithmException {
 
-
         List<List<Integer>> pendingTrns = new ArrayList<>();
-
-        List<List<Integer>> finalTransactions = new ArrayList<>();
+        List<List<Integer>> finalTransactions;
 
         for (int i = 0; i < pendingTransactions.length; i++) {
             pendingTrns.add(new ArrayList<>());
@@ -38,12 +34,11 @@ public class BlockChain {
 
         List<Block> block = new ArrayList<>();
 
-
         finalTransactions = removeAllInvalidTransactions(startBalances, pendingTrns);
-        System.out.println("all valid transactions are" + finalTransactions);
-        int k = 0;
 
+        int k = 0;
         String initialBlockHash = "0000000000000000000000000000000000000000";
+
         while (k < finalTransactions.size()) {
             List<String> blockTransactions = new ArrayList<>();
             for (int j = 0; j < blockSize && k < finalTransactions.size(); j++) {
@@ -51,22 +46,35 @@ public class BlockChain {
                 k++;
             }
 
-            StringBuilder sb1 = new StringBuilder();
-            sb1.append(initialBlockHash);
-            sb1.append(", ");
-            sb1.append("28427");
-            sb1.append(", ");
-            sb1.append(blockTransactions);
+            int nonce = 0;
 
-            String blockHash = sha1(sb1.toString());
+            String initialStringForBlockHash = buildStringForCalculatingBlockHash(initialBlockHash, nonce, blockTransactions);
+            String blockHash = sha1(initialStringForBlockHash);
 
-            System.out.print(blockTransactions + "<--");
-            Block b = new Block(initialBlockHash, "28427", blockHash, blockTransactions.toString());
+            do {
+                nonce += 1;
+                String s = buildStringForCalculatingBlockHash(initialBlockHash, nonce, blockTransactions);
+                blockHash = sha1(s);
+            } while (!blockHash.substring(0, 4).equals("0000"));
+
+            Block b = new Block(initialBlockHash, Integer.toString(nonce), blockHash, blockTransactions.toString());
             initialBlockHash = blockHash;
             block.add(b);
         }
 
-        return block.get(block.size() - 1).blockHash;
+        return block.get(block.size() - 1).blockHash + ", " + block.get(block.size() - 1).prevBlockHash + ", " + block.get(block.size() - 1).nonce + ", " + block.get(block.size() - 1).blockTransactions;
+    }
+
+    private static String buildStringForCalculatingBlockHash(String initialBlockHash, int nonce, List<String> blockTransactions) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(initialBlockHash);
+        sb.append(", ");
+        sb.append(nonce);
+        sb.append(", ");
+        sb.append(blockTransactions);
+
+        return sb.toString();
     }
 
     private static List<List<Integer>> removeAllInvalidTransactions(int[] startBalances, List<List<Integer>> pendingTrns) {
@@ -88,7 +96,7 @@ public class BlockChain {
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
 
-        System.out.println(getLatestBlock(new int[]{5, 0, 0, 0}, new int[][]{{0, 1, 5}, {1, 2, 5}, {2, 3, 5}}, 1));
+        System.out.println(getLatestBlock(new int[]{3, 10, 10, 3}, new int[][]{{3, 2, 2}, {2, 3, 5}, {3, 2, 4}, {3, 0, 2}, {1, 2, 2}}, 2));
 
     }
 
@@ -98,7 +106,7 @@ public class BlockChain {
         String blockHash;
         String blockTransactions;
 
-        public Block(String prevBlockHash, String nonce, String blockHash, String blockTransactions) {
+        Block(String prevBlockHash, String nonce, String blockHash, String blockTransactions) {
             this.prevBlockHash = prevBlockHash;
             this.nonce = nonce;
             this.blockHash = blockHash;
