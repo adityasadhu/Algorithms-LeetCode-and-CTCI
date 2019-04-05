6862112e18e3fa354890750fa32c981b9e0a3ebd
Aditya Sadhu <aditya.hf96@gmail.com>
2019-04-04 21:39:46 -0400

    moved Graph Object to a new file

--- src/Practice/Graph.java
+++ src/Practice/Graph.java
@@ -1,6 +1,50 @@
 package Practice;
 
-public class Graph {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Graph<T> {
 
+    private int vertices;
+    private Map<T, List<T>> adjacencyList;
+
+    public Graph() {
+        adjacencyList = new HashMap<>();
+    }
+
+    public void addEdge(T a, T b) {
+        if (this.adjacencyList.containsKey(a)) {
+            this.adjacencyList.get(a).add(b);
+            vertices++;
+        } else {
+            List<T> list = new ArrayList<>();
+            list.add(b);
+            this.adjacencyList.put(a, list);
+            vertices++;
+        }
+        if (this.adjacencyList.containsKey(b)) {
+            this.adjacencyList.get(b).add(a);
+        } else {
+            List<T> list = new ArrayList<>();
+            list.add(a);
+            this.adjacencyList.put(b, list);
+        }
+    }
+
+    public int getVertices() {
+        return vertices;
+    }
+
+    public T getVertex() {
+        for (Map.Entry<T, List<T>> iterator : adjacencyList.entrySet()) {
+            return iterator.getKey();
+        }
+        return null;
+    }
+
+    public Map<T, List<T>> getAdjacencyList() {
+        return adjacencyList;
+    }
 }
